58816b3f5d73954c91b65ebbb2ddc73b6d512292
liar <yyxxyyzero@163.com>
2025-10-15 23:34:27 +0800

    update

---
+++ benchmark/main.py
@@ -0,0 +1,198 @@
+import torch
+import context
+import numpy as np
+from time import perf_counter
+from constant import DATASET_PATH
+from torch import nn
+from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
+from model import Pooler, Classifier
+from rich.progress import track
+
+# proteins_dataset = TUDataset(root=DATASET_PATH, name='PROTEINS')
+
+def train_model(pooler, classifier, train_loader, optimizer, criterion, device):
+    pooler.train()
+    classifier.train()
+
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    for data in train_loader:
+        optimizer.zero_grad()
+        if data.x == None or data.x.size(1) <= 0:
+            data.x = torch.ones((data.num_nodes, 1))
+        data = data.to(device)
+        pooled = pooler(data.x, data.edge_index, data.batch)
+        out = classifier(pooled)
+        loss = criterion(out, data.y)
+        loss.backward()
+        optimizer.step()
+        
+        total_loss += loss.item()
+        pred = out.argmax(dim=1)
+        correct += (pred == data.y).sum().item()
+        total += data.y.size(0)
+    
+    return total_loss / len(train_loader), correct / total
+
+def test_model(pooler, classifier, test_loader, criterion, device):
+    pooler.eval()
+    classifier.eval()
+
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for data in test_loader:
+            if data.x == None or data.x.size(1) <= 0:
+                data.x = torch.ones((data.num_nodes, 1))
+            data = data.to(device)
+            pooled = pooler(data.x, data.edge_index, data.batch)
+            out = classifier(pooled)
+            loss = criterion(out, data.y)
+            
+            total_loss += loss.item()
+            pred = out.argmax(dim=1)
+            correct += (pred == data.y).sum().item()
+            total += data.y.size(0)
+    
+    return total_loss / len(test_loader), correct / total
+
+def run(dataset, config):
+    stamp_start = perf_counter()
+    # 加载数据集
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # print(f"设备: {device}")
+    
+    # 数据集信息
+    print(f"数据集: {dataset}")
+    # print(f"类别数: {dataset.num_classes}")
+    # print(f"节点特征维度: {dataset.num_node_features}")
+    
+    # 数据集分割
+    torch.manual_seed(42)
+    dataset = dataset.shuffle()
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset = dataset[:train_size]
+    test_dataset = dataset[train_size:]
+    
+    print(f"训练集大小: {len(train_dataset)}")
+    print(f"测试集大小: {len(test_dataset)}")
+    
+    # 数据加载器
+    batch_size = 128
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    # 模型参数
+    input_dim = dataset.num_node_features
+    hidden_dim = 64
+    # ratio = 0.8  # TopK池化比例
+    num_layers = 3
+    num_classes = dataset.num_classes
+    
+    # 创建模型
+    pooler = Pooler(input_dim, hidden_dim, num_layers=num_layers, pool_type=config['pooler'], gnn_type=config['backbone']).to(device)
+    classifier = Classifier(pooler.get_out_dim(), pooler.get_out_dim(), num_classes).to(device)
+    
+    # print(f"模型参数数量: {sum(p.numel() for p in pooler.parameters()) + sum(p.numel() for p in classifier.parameters())}")
+    
+    # 优化器和损失函数
+    optimizer = torch.optim.Adam(list(pooler.parameters()) + list(classifier.parameters()), lr=0.001)
+    criterion = nn.CrossEntropyLoss()
+    
+    # 训练循环
+    epochs = 100
+    best_test_acc = 0
+
+    train_loss_list = []
+    train_acc_list = []
+    test_loss_list = []
+    test_acc_list = []
+    
+    for epoch in track(range(epochs), description=f'训练 {dataset}'):
+        train_loss, train_acc = train_model(pooler, classifier, train_loader, optimizer, criterion, device)
+        test_loss, test_acc = test_model(pooler, classifier, test_loader, criterion, device)
+
+        train_loss_list.append(train_loss)
+        train_acc_list.append(train_acc)
+        test_loss_list.append(test_loss)
+        test_acc_list.append(test_acc)
+        
+        if test_acc > best_test_acc:
+            best_test_acc = test_acc
+        
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch {epoch+1:03d}, '
+                  f'Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, '
+                  f'Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}')
+    
+    stamp_end = perf_counter()
+
+    train_acc_mean = np.mean(train_acc_list)
+    train_acc_std = np.std(train_acc_list)
+    test_acc_mean = np.mean(test_acc_list)
+    test_acc_std = np.std(test_acc_list)
+    
+    print('训练准确率: {:.2f} ± {:.2f}, 测试准确率 {:.2f} ± {:.2f}'.format(train_acc_mean * 100, train_acc_std * 100, test_acc_mean * 100, test_acc_std * 100))
+    print(f'最佳测试准确率: {best_test_acc:.2f}')
+    print(f'总运行时间: {(stamp_end - stamp_start) * 1000:.2f} ms\n')
+    return (f'{dataset}', test_acc_mean, test_acc_std)
+
+def datasets():
+    datasets = [
+        'DD',
+        'PROTEINS',
+        'NCI1',
+        'NCI109',
+        'COX2',
+        'IMDB-BINARY',
+        'IMDB-MULTI',
+        'FRANKENSTEIN',
+        'COLLAB',
+        'REDDIT-BINARY'
+    ]
+    for i in range(len(datasets)):
+        yield TUDataset(root=DATASET_PATH, name=datasets[i])
+
+def format_result(result):
+    return f'数据集: {result[0]}, 测试准确率: {result[1] * 100:.2f}% ± {result[2] * 100:.2f}%'
+
+def save_result(result, filename, spent_time):
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(f'backbone: {config["backbone"]}, pooler: {config["pooler"]}\n')
+        for result in results:
+            f.write(f'{format_result(result)}\n')
+        f.write(f'总运行时间: {spent_time * 1000:.2f} ms\n')
+
+if __name__ == '__main__':
+    config = {
+        'backbone': 'gcn',
+        'pooler': 'sag'
+    }
+
+    all_start = perf_counter()
+    results = []
+    
+    for dataset in track(datasets()):
+        try:
+            result = run(dataset, config)
+            results.append(result)
+        except Exception as e:
+            print(f'运行 {dataset} 时出错: {e}')
+            results.append((dataset, -1, -1))
+            continue
+
+    all_end = perf_counter()
+
+    print(f'backbone: {config["backbone"]}, pooler: {config["pooler"]}')
+    for result in results:
+        print(format_result(result))
+    print(f'总运行时间: {(all_end - all_start) * 1000:.2f} ms')
+    save_result(results, f'./benchmark/result/{config["backbone"]}_{config["pooler"]}.txt', all_end - all_start)
---
+++ benchmark/model.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, GINConv
+from torch_geometric.nn.pool import TopKPooling, ASAPooling, EdgePooling, SAGPooling, PANPooling
+from torch_geometric.nn import global_mean_pool, global_max_pool
+from torch import nn
+
+activate = F.leaky_relu
+
+class Pooler(torch.nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim = None, num_layers = 3, pool_type = "topk", gnn_type = "gcn", skip_link = True):
+        super().__init__()
+        self.pool_type = pool_type
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.out_dim = out_dim
+        self.gnn_type = gnn_type
+        self.num_layers = num_layers
+        self.skip_link = skip_link
+
+        if out_dim == None:
+            out_dim = hidden_dim
+
+        if num_layers < 2:
+            raise ValueError("Number of layers must be at least 2")
+        
+        if skip_link:
+            self.real_out_dim = out_dim * num_layers * 2
+        else:
+            self.real_out_dim = out_dim * 2
+
+        #用于某些没有x的数据集
+        if self.in_dim <= 0:
+            self.in_dim = 1
+        
+        self.build_model()
+
+    def build_model(self):
+        self.conv = torch.nn.ModuleList()
+        self.pool = torch.nn.ModuleList()
+
+        for i in range(self.num_layers):
+            if i == 0:
+                in_d = self.in_dim
+                out_d = self.hidden_dim
+            # if i == self.num_layers - 1:
+            #     in_d = self.hidden_dim
+            #     out_d = self.out_dim
+            else:
+                in_d = self.hidden_dim
+                out_d = self.hidden_dim
+
+            if self.gnn_type == "gcn":
+                self.conv.append(GCNConv(in_d, out_d))
+            elif self.gnn_type == "gin":
+                self.conv.append(GINConv(nn.Linear(in_d, out_d)))
+            else:
+                raise ValueError(f'Invalid GNN type: {self.gnn_type}')
+
+        for i in range(self.num_layers):
+            in_d = self.hidden_dim
+            # out_d = self.hidden_dim
+
+            if self.pool_type.lower() == 'topk':
+                self.pool.append(TopKPooling(in_channels=in_d))
+            # elif self.pool_type.lower() == 'mean':
+            #     self.pool.append(torch.nn.AvgPool1d(kernel_size=1))
+            # elif self.pool_type.lower() == 'max':
+            #     self.pool.append(torch.nn.MaxPool1d(kernel_size=1))
+            elif self.pool_type.lower() == 'asap':
+                self.pool.append(ASAPooling(in_channels=in_d))
+            elif self.pool_type.lower() == 'edge':
+                self.pool.append(EdgePooling(in_channels=in_d))
+            elif self.pool_type.lower() == 'sag':
+                self.pool.append(SAGPooling(in_channels=in_d))
+            elif self.pool_type.lower() == 'pan':
+                self.pool.append(PANPooling(in_channels=in_d))
+            else:
+                raise ValueError(f'Invalid pool type: {self.pool_type}')
+
+    def get_out_dim(self):
+        return self.real_out_dim
+
+    def forward(self, x, edge_index, batch = None):
+        if batch is None:
+            batch = torch.zeros(x.size(0), dtype=torch.long, device=x.device)
+        
+        # 存储各层特征
+        layer_features = []
+        original_x = x
+
+        for i in range(self.num_layers):
+            x = self.conv[i](x, edge_index)
+            x = activate(x)
+            
+            if self.pool_type.lower() == 'topk':
+                x, edge_index, _, batch, _, _ = self.pool[i](x, edge_index, batch=batch)
+            elif self.pool_type.lower() == 'asap':
+                x, edge_index, _, batch, _ = self.pool[i](x, edge_index, batch=batch)
+            elif self.pool_type.lower() == 'edge':
+                x, edge_index, batch, _ = self.pool[i](x, edge_index, batch=batch)
+            elif self.pool_type.lower() == 'sag':
+                x, edge_index, _, batch, _, _ = self.pool[i](x, edge_index, batch=batch)
+            elif self.pool_type.lower() == 'pan':
+                adj_matrix = torch.sparse_coo_tensor(edge_index, torch.ones(edge_index.size(1)).to(x.device), (x.size(0), x.size(0)))
+                x, edge_index, _, batch, _, _ = self.pool[i](x, adj_matrix, batch=batch)
+            
+            global_feat = (global_max_pool(x, batch), global_mean_pool(x, batch))
+            global_feat = torch.cat([global_feat[0], global_feat[1]], dim=1)
+            layer_features.append(global_feat)
+        
+        x = torch.cat(layer_features, dim=1)
+
+        return x
+    
+class Classifier(torch.nn.Module):
+    def __init__(self, in_dim, hidden_dim, num_classes):
+        super().__init__()
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+        self.num_classes = num_classes
+
+        self.liner0 = torch.nn.Linear(in_dim, hidden_dim)
+        self.liner1 = torch.nn.Linear(hidden_dim, num_classes)
+
+        # self._initialize_weights()
+    
+    def _initialize_weights(self):
+        # Kaiming/He 初始化（适合ReLU族激活函数）
+        torch.nn.init.kaiming_uniform_(self.liner0.weight)
+        torch.nn.init.kaiming_uniform_(self.liner1.weight)
+    
+        # 初始化偏置为0
+        if self.liner0.bias is not None:
+            nn.init.constant_(self.liner0.bias, 0)
+        if self.liner1.bias is not None:
+            nn.init.constant_(self.liner1.bias, 0)
+
+    def forward(self, x):
+        x = activate(self.liner0(x))
+        x = self.liner0(x)
+        # x = activate(x)
+        x = F.leaky_relu(x)
+        x = torch.dropout(x, p=0.4, train=self.training)
+        x = self.liner1(x)
+
+        return x
