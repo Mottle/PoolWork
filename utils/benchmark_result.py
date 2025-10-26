291d9cc885cb624bcc26e3de5fcce5e850b44d1c
liar <yyxxyyzero@163.com>
2025-10-26 23:18:15 +0800

    update

---
+++ utils/benchmark_result.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class BenchmarkResult:
+    def __init__(self, name: str = 'accuarcy'):
+        self._result: list[float] = []
+
+    def append(self, result: float):
+        self._result.append(result)
+
+    def get_max(self, last = None) -> float:
+        if last is None:
+            return max(self._result)
+        else:
+            last_list = self._result[-last:]
+            return max(last_list)
+
+    def get_mean(self, last = None) -> float:
+        if last is None:
+            return np.mean(self._result)
+        else:
+            last_list = self._result[-last:]
+            return np.mean(last_list)
+
+    def get_std(self, last = None) -> float:
+        if last is None:
+            return np.std(self._result)
+        else:
+            last_list = self._result[-last:]
+            return np.std(last_list)
+        
+    
+    def format(self, name: str = None, percent: bool = True, last = None) -> str:
+        if name is None:
+            name = self._name
+        
+        if percent == True:
+            return f'{name}: {self.get_mean(last) * 100:.2f}% ± {self.get_std(last) * 100:.2f}% (best = {self.get_max(last) * 100:.2f}%)'
+        else:
+            return f'{name}: {self.get_mean(last):.4f} ± {self.get_std(last):.4f} (best = {self.get_max(last):.4f})'
+    
+    def get_data(self) -> list[float]:
+        return self._result
+    
+    def get(self, idx: int) -> float:
+        return self._result[idx]
